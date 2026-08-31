d41170abd492899fdb93c8a7c96b4e352f264798
1234 <dddu809@gmail.com>
2026-08-31 20:52:45 +0100

    修复 BookCollectionActivity 缺少 EditText 导入导致编译失败

--- app/src/main/java/io/legado/app/ui/main/bookshelf/BookCollectionActivity.kt
+++ app/src/main/java/io/legado/app/ui/main/bookshelf/BookCollectionActivity.kt
@@ -7,6 +7,7 @@ import android.view.HapticFeedbackConstants
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.activity.addCallback
 import androidx.core.content.ContextCompat
